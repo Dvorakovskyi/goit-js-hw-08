26ecb69765c92bbb2a3f9e0a58881680988c67bc
Volodymyr <vladimir.dvorakovskyi@gmail.com>
2023-03-09 16:01:29 +0200

    Виконав завдання 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,5 +3,16 @@ import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const galleryEl = document.querySelector('.gallery');
 
-console.log(galleryItems);
+const galleryImg = galleryItems.map(({ preview, original, description, }) =>     
+`<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+).join('');
+galleryEl.insertAdjacentHTML('afterbegin', galleryImg);
+
+new SimpleLightbox('.gallery__item', {
+    captionsData: 'alt',
+    captionDelay: 250,
+})
