da7ce0a054bda083a853444adb91580c98f64961
Volodymyr <vladimir.dvorakovskyi@gmail.com>
2023-03-11 18:27:20 +0200

    Виконав Завдання 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const videoEl = document.querySelector('#vimeo-player');
+
+const STOREAGE_CURRENT_TIME_KEY = 'videoplayer-current-time';
+
+const videoPlayer = new Player(videoEl);
+
+const handleSavedCurrentTime = event => {
+  localStorage.setItem(STOREAGE_CURRENT_TIME_KEY, event.seconds);
+};
+
+videoPlayer.on('timeupdate', throttle(handleSavedCurrentTime, 1000));
+
+const currentTime = localStorage.getItem(STOREAGE_CURRENT_TIME_KEY)
+    ? localStorage.getItem(STOREAGE_CURRENT_TIME_KEY)
+    : 0;
+
+videoPlayer.setCurrentTime(currentTime);
 
