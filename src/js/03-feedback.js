b06fc551115a8f9773127dd85e4537047433f6b7
Volodymyr <vladimir.dvorakovskyi@gmail.com>
2023-03-11 19:04:33 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,12 @@
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    input: document.querySelector('.feedback-form input'),
+    textarea: document.querySelector('.feedback-form textarea'),
+}
+
+
+
+
+console.log(refs.textarea);
+const FEEDBACK_FORM_KEY = 'feedback-form-state';
 
