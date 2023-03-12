df538635076c3d680810c2445fda30a3a0598297
Volodymyr <vladimir.dvorakovskyi@gmail.com>
2023-03-12 15:03:37 +0200

    Виконав завдання 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,12 +1,43 @@
-const refs = {
-    form: document.querySelector('.feedback-form'),
-    input: document.querySelector('.feedback-form input'),
-    textarea: document.querySelector('.feedback-form textarea'),
+import throttle from 'lodash.throttle';
+
+const formEl = document.querySelector('.feedback-form');
+const emailEl = document.querySelector('.feedback-form input');
+const messageEl = document.querySelector('.feedback-form textarea');
+
+const FEEDBACK_FORM_KEY = 'feedback-form-state';
+const formData = {};
+
+const handleFormData = (event) => {
+    formData[event.target.name] = event.target.value;
+
+    const formDataJson = JSON.stringify(formData);
+
+    localStorage.setItem(FEEDBACK_FORM_KEY, formDataJson);
 }
 
+formEl.addEventListener('input', throttle(handleFormData, 500));
 
+const getLocalStorage = () => {
+    const isLocalStorage = localStorage.getItem(FEEDBACK_FORM_KEY);
 
+    if (isLocalStorage) {
+        const formDataParsed = JSON.parse(isLocalStorage);
 
-console.log(refs.textarea);
-const FEEDBACK_FORM_KEY = 'feedback-form-state';
+        emailEl.value = formDataParsed.email;
+        messageEl.value = formDataParsed.message;
+    }
+}
 
+getLocalStorage();
+
+const handleFormSubmit = (event) => {
+    event.preventDefault();
+
+    event.target.reset();
+
+    localStorage.removeItem(FEEDBACK_FORM_KEY);
+
+    console.log(formData);
+}
+
+formEl.addEventListener('submit', handleFormSubmit);
